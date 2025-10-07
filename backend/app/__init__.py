4f14d695f6e3742748d993b66b70b0830c5b94d4
david26044 <davidcarreno767@gmail.com>
2025-10-06 19:22:12 -0500

    Configuración inicial con .gitignore y estructura base

---
+++ backend/app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from .config import Config
+
+db = SQLAlchemy()
+jwt = JWTManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    CORS(app)
+
+    db.init_app(app)
+    jwt.init_app(app)
+
+    @app.route("/")
+    def home():
+        return "✅ Flask conectado correctamente"
+    return app
